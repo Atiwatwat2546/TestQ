c14e38f5af0fcef57734918793462953c9b4a8c8
Atiwatwat2546 <Atiwatwat2546@gmail.com>
2025-07-09 16:04:23 +0700

    code

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import CurrencyConverter from "@/pages/currency-converter";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={CurrencyConverter} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
